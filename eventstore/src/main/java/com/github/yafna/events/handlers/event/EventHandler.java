bc50a8be0fe37e08e9d6bc3972fde7fd551685d5
Demetrio Kirodis <kirodis@github.com>
2017-06-14 00:43:00 +0200

    Add event pipeline and dispatcher

---
+++ eventstore/src/main/java/com/github/yafna/events/handlers/event/EventHandler.java
@@ -0,0 +1,24 @@
+package com.github.yafna.events.handlers.event;
+
+import com.github.yafna.events.EmittedEvent;
+import com.github.yafna.events.Event;
+import com.github.yafna.events.handlers.Handler;
+
+import java.util.stream.Stream;
+
+/**
+ * A handler that is allowed to emit other events, but is not attached to aggregate.
+ *
+ * @param <T> Event type
+ */
+@FunctionalInterface
+public interface EventHandler<T> extends Handler<T> {
+
+    /**
+     * Apply event
+     * @param meta metadata of the event being processed
+     * @param payload event payload
+     * @return events emitted as a result of processing this event
+     */
+    Stream<EmittedEvent<?>> apply(Event meta, T payload);
+}
