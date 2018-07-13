17f967955ffbe003a60a62492d70db434c431036
Demetrio Kirodis <kirodis@github.com>
2018-07-14 00:49:18 +0200

    In Handler, update javadocs

--- eventstore/src/main/java/com/github/yafna/events/annotations/Handler.java
+++ eventstore/src/main/java/com/github/yafna/events/annotations/Handler.java
@@ -1,13 +1,22 @@
 package com.github.yafna.events.annotations;
 
+import com.github.yafna.events.Event;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
  * Marks methods of aggregate object that describe event handlers.
  * Such methods must match one of the following contracts:
- * a) One argument of type assignment compatible with event payload
- * b) Two arguments, first is event metadata, second is
+ *
+ * <p> 1. One argument, {@link Event} (which carries event metadata).
+ * No attempt to read payload is made when invoking it.
+ *
+ * <p> 2. One argument, event payload. Argument type must be assignment compatible with event payload type as
+ *
+ * <p> 3. Two arguments, first is event metadata, second is event payload.
+ *
+ * <p> Note that event payload argument type must be assignment compatible with EvType event payload type as
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Handler {
