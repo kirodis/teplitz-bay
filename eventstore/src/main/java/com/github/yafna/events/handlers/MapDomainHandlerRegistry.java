13b51c43042edaac4b25e9c008bc9047069cafcb
Demetrio Kirodis <kirodis@github.com>
2018-07-14 17:40:19 +0200

    In MapDomainHandlerRegistry, add javadoc and make the map final

--- eventstore/src/main/java/com/github/yafna/events/handlers/MapDomainHandlerRegistry.java
+++ eventstore/src/main/java/com/github/yafna/events/handlers/MapDomainHandlerRegistry.java
@@ -7,12 +7,15 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * A trivial handler registry, backed up by a map
- * @param <A>
+ * A trivial handler registry, backed up by a map.
+ * Provides neat generic signatures and encapsulates the map to prevent its modification.
+ * Does not provide any validation features - it should be done before instantiating it.
+ * 
+ * @param <A> aggregate type
  */
 @AllArgsConstructor
 public class MapDomainHandlerRegistry<A> implements DomainHandlerRegistry<A> {
-    private Map<String, List<DomainHandler<A, ?>>> handlers;
+    private final Map<String, List<DomainHandler<A, ?>>> handlers;
 
     @SuppressWarnings("unchecked")
     @Override
