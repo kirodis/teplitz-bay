a175703c5ef2aa58feadcbf1e8bcaeda96a26d64
Demetrio Kirodis <kirodis@github.com>
2018-07-14 21:13:43 +0200

    In EventPipeline, javadoc and cosmetic fixes

--- eventstore/src/main/java/com/github/yafna/events/pipelines/EventPipeline.java
+++ eventstore/src/main/java/com/github/yafna/events/pipelines/EventPipeline.java
@@ -14,9 +14,22 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.Optional;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ExecutorService;
 import java.util.stream.Stream;
 
+/**
+ * Processes events using {@link EventHandler}. 
+ * As such events can emit other events, they should not be reprocessed to prevent duplicating emitted events.
+ * To ensure it, the events emitted by the handler are persisted together with the special "handled" 
+ * event that registers the fact that processing is complete. 
+ * 
+ * When instantiated, pipeline connects to {@link EventDispatcher} and attempts to gathers all the events in the given
+ * time interval that do not have a matching "handled" event.
+ * TODO Implement that filtering, now pipeline ignores "handled" records 
+ * 
+ * @param <A> Aggregate type
+ * @param <T> Event type
+ */
 @Slf4j
 public class EventPipeline<A, T extends DomainEvent<A>> {
     /** Aggregate name under which service events recording state of the handler are saved to event store */
@@ -24,20 +37,14 @@ public class EventPipeline<A, T extends DomainEvent<A>> {
     /** Event type used to mark handled events */
     public final static String EVENT_TYPE_HANDLED = "handled";
 
-    private final ForkJoinPool executor;
+    private final ExecutorService executor;
     private final EventDispatcher dispatcher;
     private final String origin;
     private final String handlerId;
     private final EventHandler<T> handler;
 
     public EventPipeline(
-            EventDispatcher dispatcher, Class<T> eventType, EventHandler<T> handler, String handlerId, Clock clock
-    ) {
-        this(dispatcher, eventType, handler, handlerId, clock, new ForkJoinPool());
-    }
-
-    public EventPipeline(
-            EventDispatcher dispatcher, Class<T> eventType, EventHandler<T> handler, String handlerId, Clock clock, ForkJoinPool executor
+            EventDispatcher dispatcher, Class<T> eventType, EventHandler<T> handler, String handlerId, Clock clock, ExecutorService executor
     ) {
         origin = PayloadUtils.origin(eventType);
         this.executor = executor;
